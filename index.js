7df4ad0f6977bade53dcf4abbb88e7222bab2442
deividasdul <deividas.dul@gmail.com>
2024-03-13 18:31:33 +0200

    Last commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var gameHasStarted = false;
 var randomChosenHole;
 var randomChosenAppearTime;
+var hole;
+var score = 0;
+var userClickedHole;
+var scream = new Audio("./sounds/blue.mp3");
 var holes = [
   "one",
   "two",
@@ -12,9 +16,6 @@ var holes = [
   "eight",
   "nine",
 ];
-
-var hole;
-var score = 0;
 
 $(document).keydown((event) => {
   if (event.key === "s" && !gameHasStarted) {
@@ -31,7 +32,7 @@ $(document).keydown((event) => {
 });
 
 $(".grid-container div").click(function () {
-  var userClickedHole = $(this).attr("id");
+  userClickedHole = $(this).attr("id");
 
   console.log(userClickedHole);
   console.log(randomChosenHole);
@@ -46,106 +47,20 @@ $(".grid-container div").click(function () {
 
     randomChosenAppearTime = Math.floor(Math.random() * 1000) + 1;
     appear(hole, randomChosenAppearTime);
-  }
-  else {
+  } else {
     console.log("Le epic fail");
   }
 });
 
-var scream = new Audio("./sounds/blue.mp3");
-
 // Plays Sounds When User Clicks a Hole
 function playSound(clickedHole) {
-  switch (clickedHole) {
-    case "one":
-      scream.play();
-      break;
-    case "two":
-      scream.play();
-      break;
-    case "three":
-      scream.play();
-      break;
-    case "four":
-      scream.play();
-      break;
-    case "five":
-      scream.play();
-      break;
-    case "six":
-      scream.play();
-      break;
-    case "seven":
-      scream.play();
-      break;
-    case "eight":
-      scream.play();
-      break;
-    case "nine":
-      scream.play();
-      break;
-  }
+  scream.play();
 }
 
 // Makes Wild Peciulis to Appear and Dissapear (Randomized)
 function appear(chosenHole, timer) {
-  switch (chosenHole) {
-    case "one":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "two":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "three":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "four":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "five":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "six":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "seven":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "eight":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    case "nine":
-      $("#" + chosenHole).addClass("active-hole");
-      setTimeout(function () {
-        $("#" + chosenHole).removeClass("active-hole");
-      }, timer);
-      break;
-    default:
-      console.log("Error");
-      break;
-  }
+  $("#" + chosenHole).addClass("active-hole");
+  setTimeout(function () {
+    $("#" + chosenHole).removeClass("active-hole");
+  }, timer);
 }
